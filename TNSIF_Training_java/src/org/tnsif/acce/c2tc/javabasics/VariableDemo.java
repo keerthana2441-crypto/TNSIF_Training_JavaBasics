4a3ed0eba30a04e0d3613cd81f425a6b62927836
DELL <DELL@DESKTOP-P3HH3H9>
2026-07-16 10:59:15 +0530

    first commit

---
+++ TNSIF_Training_java/src/org/tnsif/acce/c2tc/javabasics/VariableDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.acce.c2tc.javabasics;
+
+public class VariableDemo {
+	int price=10;
+	void print() 
+	{
+		String msg="hello";
+		System.out.println(msg);
+	}
+	static String message="Hello Student";
+
+	public static void main(String[] args) {
+		VariableDemo obj=new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
